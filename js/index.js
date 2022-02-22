b7e8f416ce1e49f4be320a2e3d5288abf6302d7f
May <maymchen1@gmail.com>
2022-02-22 15:53:01 -0500

    Changed alert message and mainDiv

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-alert('hello world!')
+alert('Welcome! Shop our new arrivals')
+/** Globals **/
+
+/** NODE Getters **/
+const mainDiv = () => document.getElementById("main");
+
+/*****************/
+
+
+
+
+
+/** WHEN THE DOM LOADS **/
