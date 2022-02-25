37bddbcccc5a0a684075b45806a212ae582ed105
May <maymchen1@gmail.com>
2022-02-24 20:10:02 -0500

    search page

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-//alert('Welcome! Shop our new arrivals')//
-
 /**  
-  * First of all, you want to explain to yourslef the full feature
+  * First of all, you want to explain to yourself the full feature
   * Break down feature with (3 question rule):
         * At what time can I do this event? (DOM content loaded)
         * What's going to trigger this event? (click)
         * effect (display home page)
   **/
-/** Globals **/
 
+/** Globals **/
+const baseUrl = 'http://localhost:3000'
+let inventory = [];
 /** NODE Getters **/
 const mainDiv = () => document.getElementById("main");
 const homePageLink = () => document.getElementById('home-page-link');
 const wishlistLink = () => document.getElementById('wishlist-link');
-const cartLink = () => document.getElementById('cart-link');
+const searchLink = () => document.getElementById('search-link');
 /** Templates **/
 const homePageTemplate = () => {
     return `
@@ -51,37 +51,59 @@ const wishlistTemplate = () => {
        </tbody>
      </table>  `
 }
-const cartTemplate = () => {
-    return `
-    <h1>Your Cart</h1>
-    <table class="highlight">
-       <thead>
-         <tr>
-             <th>Item Name</th>
-             <th>Item Size</th>
-             <th>Item Price</th>
-         </tr>
-       </thead>
+const searchTemplate = () => {
+    const h1 = document.createElement('h1');
+    const form = document.createElement('form');
 
-       <tbody>
-         <tr>
-           <td>Dress</td>
-           <td>XS</td>
-           <td>$50.00</td>
-         </tr>
-         <tr>
-           <td>Jeans</td>
-           <td>2</td>
-           <td>$30.00</td>
-         </tr>
-         <tr>
-           <td>Shirt</td>
-           <td>L</td>
-           <td>$15.00</td>
-         </tr>
-       </tbody>
-     </table>  `
-}
+    const itemDiv = document.createElement('div');
+    const itemInput = document.createElement ('input');
+    const itemLabel = document.createElement ('label');
+    
+    const sizeDiv = document.createElement('div');
+    const sizeInput = document.createElement ('input');
+    const sizeLabel = document.createElement ('label');
+
+    const priceDiv = document.createElement('div');
+    const priceInput = document.createElement ('input');
+    const priceLabel = document.createElement ('label');
+
+    h1.className = 'center-align';
+    itemDiv.className ='input-field';
+    sizeDiv.className ='input-field';
+    priceDiv.className ='input-field';
+
+    itemInput.setAttribute ('id', 'date');
+    itemInput.setAttribute ('type', 'text');
+    itemInput.setAttribute ('for', 'date');
+
+    sizeInput.setAttribute ('id', 'date');
+    sizeInput.setAttribute ('type', 'text');
+    sizeInput.setAttribute ('for', 'date');
+
+    priceInput.setAttribute ('id', 'date');
+    priceInput.setAttribute ('type', 'text');
+    priceInput.setAttribute ('for', 'date');
+
+    h1.innerText = 'Search';
+    itemLabel.innerText ='Item';
+    sizeLabel.innerText = 'Size';
+    priceLabel.innerText = 'Price';
+
+    itemDiv.appendChild(itemInput);
+    itemDiv.appendChild(itemLabel);
+    sizeDiv.appendChild(sizeInput);
+    sizeDiv.appendChild(sizeLabel);
+    priceDiv.appendChild(priceInput);
+    priceDiv.appendChild(priceLabel);
+
+    form.appendChild(itemDiv);
+    form.appendChild(sizeDiv);
+    form.appendChild(priceDiv);
+  
+    mainDiv().appendChild(h1);
+    mainDiv().appendChild(form);
+  }
+
  /** Renderers **/
 
 const renderHomePage = () => {
@@ -92,10 +114,16 @@ const renderWishlist = () => {
     mainDiv().innerHTML = wishlistTemplate();
 }
 
-const renderCart = () => {
-    mainDiv().innerHTML = cartTemplate();
+const renderSearch = () => {
+    mainDiv().innerHTML = searchTemplate();
 }
 /** Events **/
+const loadInventory = () => {
+    fetch(baseUrl + '/inventory')
+    .then(resp => resp.json())
+    .then(data => inventory = data)
+}
+
 const homePageLinkEvent = () => {
     homePageLink().addEventListener('click', (e)=> {
         e.preventDefault();
@@ -108,12 +136,13 @@ const wishlistEvent = () => {
         renderWishlist();
     })
 }
-const cartEvent = () => {
-    cartLink().addEventListener('click', (e)=> {
+const searchEvent = () => {
+    searchLink().addEventListener('click', (e)=> {
         e.preventDefault();
-        renderCart();
+        renderSearch();
     })
 }
+
 /*****************/
 
 
@@ -125,5 +154,5 @@ document.addEventListener('DOMContentLoaded', () =>{
   // renderHomePage();
   homePageLinkEvent();
   wishlistEvent();
-  cartEvent();
+  searchEvent();
 })
